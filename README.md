8047c35008d74b62207adfad8fe4556d5de808ab
Victor Wibisono <hi@victor.codes>
2019-10-20 19:16:17 +0800

    Some stability attained.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # ms
 
-Clickstream Data Pipeline
+Clickstream Data Pipeline.
 
 ### To start
 
 Run `bin/start` from the terminal to start.
+
+### Connecting to KSQL server
+
+```
+/Users/victor/Downloads/from_desktop/drc-demo/confluent-5.3.0/bin/ksql http://localhost:8088
+```
---
+++ ksql/clickstream-schema.sql
@@ -0,0 +1,6 @@
+set 'commit.interval.ms'='2000';
+set 'cache.max.bytes.buffering'='10000000';
+set 'auto.offset.reset'='earliest';
+
+-- 1. SOURCE of ClickStream
+CREATE STREAM clickstream (session_id varchar, user_id varchar, partner_name varchar, page_type varchar, init_session varchar, date bigint, user_agent varchar, city varchar, current_url varchar, referrer varchar, language varchar) with (kafka_topic = 'clickstream', value_format = 'json', timestamp='date');
